3d5cddddc1becd93f172f76b8a147eba2828c780
InsertAReallyCreativeNameHere <60645493+InsertAReallyCreativeNameHere@users.noreply.github.com>
2020-02-17 21:08:38 +1100

    Code editing and minor changes.

---
+++ RoboticsController/Assets/Scripts/CameraUpdater.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class CameraUpdater : MonoBehaviour
+{
+    public GameObject trackedObject;
+    [SerializeField] new Camera camera;
+
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    void Update()
+    {
+        camera.transform.parent.position = trackedObject.transform.position;
+        Thread thread = new Thread(async () => await LateCameraUpdate(trackedObject.transform));
+        thread.Start(trackedObject.transform);
+    }
+
+    public async Task LateCameraUpdate(Transform transformOfTrackedObj)
+    {
+        await Task.Delay(2000);
+        camera.transform.parent.localRotation = Quaternion.Euler(0, transformOfTrackedObj.localEulerAngles.y, 0);
+    }
+}
